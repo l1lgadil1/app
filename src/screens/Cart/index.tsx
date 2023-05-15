bafd3161222ddbd7cd1936343fd5773f8e52bd30
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-05-15 22:21:44 +0600

    Cart suggestions

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -8,21 +8,32 @@ import CartBottomComponent from "../../components/cart/cartBottomComponent";
 const Cart = ({ navigate }: any) => {
   const cartItems = useAppSelector(state => state.cart.cart);
 
+  const renderCart = cartItems.length > 0
+    ? <ScrollView>
+      {
+        cartItems.map((item) => (
+          <CartProduct
+            title={item.title} images={item.images} thumbnail={item.thumbnail} category={item.category}
+            brand={item.brand} rating={item.rating} discountPercentage={item.discountPercentage}
+            price={item.price} description={item.description} stock={item.stock} id={item.id}
+            count={item.count} />
+        ))
+      }
+    </ScrollView>
+    : <View style={styles.empty_cart}>
+      <Text style={styles.empty_cart_text}>
+        Your cart is empty
+      </Text>
+      <Text style={styles.empty_cart_text}>
+        :(
+      </Text>
+      <Text style={styles.empty_cart_description}>Add items to continue shopping</Text>
+    </View>;
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {
-          cartItems.map((item) => (
-            <CartProduct
-              title={item.title} images={item.images} thumbnail={item.thumbnail} category={item.category}
-              brand={item.brand} rating={item.rating} discountPercentage={item.discountPercentage}
-              price={item.price} description={item.description} stock={item.stock} id={item.id}
-              count={item.count} />
-          ))
-        }
-      </ScrollView>
-      <CartBottomComponent cartItems={cartItems} />
+      {renderCart}
+      {cartItems.length > 0 && <CartBottomComponent cartItems={cartItems} />}
     </View>
   );
 };
@@ -30,8 +41,22 @@ const Cart = ({ navigate }: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  empty_cart: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  empty_cart_text: {
+    fontSize:24,
+    fontWeight:'500'
+  },
+  empty_cart_description:{
+    marginVertical:15,
+    color:'gray',
+    fontSize:14,
+    fontWeight:'400'
   }
-
 });
 
 export default Cart;
